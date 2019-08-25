f416300e4588f4e99de727ace67e1370189a3bf6
amitozs999 <ekas0001@gmail.com>
2019-08-25 18:27:06 +0530

    commit

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,6 +1,14 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req, res){
-    
-    return res.render('home', {
-        title: "using homecontroller"
-    });
+
+
+    // populate the user of each post
+    Post.find({}).populate('user').exec(function(err, posts){
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts:  posts
+        });
+    })
+
 }
