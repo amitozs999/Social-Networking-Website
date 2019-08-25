7036b3b86821baa4e7b09812b217607900f19ca1
amitozs999 <ekas0001@gmail.com>
2019-08-25 20:19:01 +0530

    commit

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ var router=express.Router();
 
 var homeController=require('../controller/home_controller')
 
+
 router.get('/',homeController.home);
 
 router.use('/user',require('./users'));
 router.use('/posts', require('./posts'));
+router.use('/comments', require('./comments'));
 
 module.exports=router;
 
