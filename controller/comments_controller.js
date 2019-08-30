565ba1bb93249794449f8fcbee9060f2758fffb8
amitozs999 <ekas0001@gmail.com>
2019-08-30 17:02:05 +0530

    deleting comments using ajax without refreshing page

--- controller/comments_controller.js
+++ controller/comments_controller.js
@@ -51,6 +51,15 @@ module.exports.destroy = async function(req, res){
             comment.remove();
 
             let post = Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}});
+               // send the comment id which was deleted back to the views
+               if (req.xhr){
+                return res.status(200).json({
+                    data: {
+                        comment_id: req.params.id
+                    },
+                    message: "Post deleted"
+                });
+            }
             req.flash('success', 'Comment deleted!');
 
             return res.redirect('back');
