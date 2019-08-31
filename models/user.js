58a040f6aa66ac513e813cc675ebd21a3229f680
amitozs999 <ekas0001@gmail.com>
2019-08-31 14:33:48 +0530

    uploading dp using multer

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const multer=require('multer');
 const path=require('path');
-const dppath=path.join('/uploads/user/dp')
+const dp_path=path.join('/uploads/user/dp')
 
 var userSchema = new mongoose.Schema({
     email: {
@@ -17,11 +17,27 @@ var userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
+    },
+    dp:{
+        type:String
     }
 }, {
     timestamps: true
 });
 
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname,'..',dp_path))   //user.js(__dirname)->models->dp_path
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now())
+    }
+  })
+   
+
+// static
+userSchema.statics.uploadedDp = multer({storage:  storage}).single('dp');
+userSchema.statics.dpPath = dp_path;
 
 var User = mongoose.model('User', userSchema);
 
