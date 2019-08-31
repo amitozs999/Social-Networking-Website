56ec7e43828b184f11d89f1240a3b445e8e8dd0d
amitozs999 <ekas0001@gmail.com>
2019-08-31 21:35:25 +0530

    generating token after creating session and use it for deleting posts

--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -1,11 +1,18 @@
 const express = require('express');
 
+
 const router = express.Router();
+const passport = require('passport');
+
 const postsApi = require("../../../controller/api/v1/posts_api");
 
 
 router.get('/', postsApi.index);
-router.delete('/:id', postsApi.destroy);
+router.delete('/:id',passport.authenticate('jwt', {session: false}), postsApi.destroy);
+
+//http://localhost:8010/api/v1/posts/5d638f743c1f873fc8daab2c  (delete)
+//paste token in headers   
+// key->authorization   value->  Bearer token
 
 
 
--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -6,4 +6,11 @@ const usersApi = require('../../../controller/api/v1/users_api');
 
 router.post('/create-session', usersApi.createSession);
 
+
+//http://localhost:8010/api/v1/user/create-session 
+//enter email password in body
+//then copy token
+
+
+
 module.exports = router;
