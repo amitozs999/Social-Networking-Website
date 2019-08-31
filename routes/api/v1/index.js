9ef98479b136c11b218d7c96a2b4ebeeb9b71743
amitozs999 <ekas0001@gmail.com>
2019-08-31 20:38:59 +0530

    generating jwt token after creating session using user api

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -3,5 +3,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/posts', require('./posts'));
+router.use('/user', require('./users'));
 
 module.exports = router;
