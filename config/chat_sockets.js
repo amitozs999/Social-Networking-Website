a5c7db99c50ec96ab1d8b8c4ff15f8f012716bd2
amitozs999 <ekas0001@gmail.com>
2019-09-02 18:42:47 +0530

    implementing chatting feature on joining romm

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -1,4 +1,5 @@
 
+
 module.exports.chatSockets = function(socketServer){
     let io = require('socket.io')(socketServer);
 
@@ -16,7 +17,12 @@ module.exports.chatSockets = function(socketServer){
             socket.join(data.chatroom);
 
             io.in(data.chatroom).emit('user_joined', data);
-        })
+        });
+
+        // CHANGE :: detect send_message and broadcast to everyone in the room
+        socket.on('send_message', function(data){
+            io.in(data.chatroom).emit('receive_message', data);
+        });
 
     });
 
