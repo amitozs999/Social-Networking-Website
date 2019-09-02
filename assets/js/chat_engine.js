bc3c560cf8319199dc9d04ff44a2194dca3288fb
amitozs999 <ekas0001@gmail.com>
2019-09-02 17:40:31 +0530

    joining chat room

---
+++ assets/js/chat_engine.js
@@ -0,0 +1,34 @@
+class ChatEngine{
+    constructor(chatBoxId, userEmail){
+        this.chatBox = $(`#${chatBoxId}`);
+        this.userEmail = userEmail;
+
+        this.socket = io.connect('http://localhost:8000');
+
+        if (this.userEmail){
+            this.connectionHandler();
+        }
+
+    }
+
+
+    connectionHandler(){
+        let self = this;
+
+        this.socket.on('connect', function(){
+            console.log('connection established using sockets...!');
+
+
+            self.socket.emit('join_room', {
+                user_email: self.userEmail,
+                chatroom: 'my chat room 1'
+            });
+
+            self.socket.on('user_joined', function(data){
+                console.log('a user joined!', data);
+            })
+
+
+        });
+    }
+}
